8a7ccde0194631ddc1861fbcc20144437e9a3d2e
samuca78 <81726687+samuca78@users.noreply.github.com>
2021-06-28 19:55:30 -0300

    update afk.py

--- plugins/afk.py
+++ plugins/afk.py
@@ -139,9 +139,6 @@ async def respostas(message: Message) -> None:
     user_dict = await message.client.get_user_dict(user_id)
     afk_time = time_formatter(round(time.time() - TIME))
     coro_list = []
-
-    client = message.client
-    chat_id = message.chat.id
     if user_id in USERS:
         if not (USERS[user_id][0] + USERS[user_id][1]) % randint(2, 4):
             match = _TELE_REGEX.search(REASON)
