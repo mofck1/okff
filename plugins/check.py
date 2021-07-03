84987f15b8f820952d5cfa1563c79c9725aa38c1
Apple <scarotrhy@gmail.com>
2021-07-03 08:52:47 -0300

    @applled

--- plugins/check.py
+++ plugins/check.py
@@ -1,3 +1,4 @@
+""" Módulo de testes para o @applled com fins de aprendizado """
 import os
 import asyncio
 from datetime import datetime
@@ -9,7 +10,7 @@ from userge.utils import get_file_id, rand_array
 
 LOGGER = userge.getLogger(__name__)
 CHECKS = (
-     "https://telegra.ph/file/b2fda41d76cd798d4c368.gif",
+    "https://telegra.ph/file/b2fda41d76cd798d4c368.gif",
     "https://telegra.ph/file/43901682e8a936d76572e.gif",
     "https://telegra.ph/file/140d286c155894093c250.gif",
     "https://telegra.ph/file/ebfb744d7a25736ef09f5.gif", 
@@ -18,7 +19,7 @@ CHECKS = (
 @userge.on_cmd(
     "check",
     about={
-        "header": "Módulo teste - @applled",
+        "header": "Módulo teste para o @applled",
     },
     del_pre=True,
     allow_channels=False,
@@ -27,7 +28,7 @@ CHECKS = (
 async def apple(message: Message):
     await message.edit("**Iniciando checagem...**\nAguarde o resultado, Mestre...", log=__name__)
     photo = f"""{random.choice(CHECKS)}"""
-    texto = "__I'm Online, @appled!__"
+    texto = "<u>I'm Online</u>, @applled!"
     await message.client.send_animation(
                          message.chat.id, 
                          animation=photo, 
