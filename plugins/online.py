dec4d38c39391db7462d8a793f2c9eed6357198f
Apple <scarotrhy@gmail.com>
2021-07-03 04:53:46 -0300

    Update online.py

--- plugins/online.py
+++ plugins/online.py
@@ -4,7 +4,12 @@ from PIL import Image
 import asyncio
 from userge import Message, userge
 from userge.utils import get_file_id, rand_array
-
+from pyrogram.types import (
+    CallbackQuery,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    User,
+)
 
 LOGGER = userge.getLogger(__name__)
 
@@ -17,9 +22,24 @@ LOGGER = userge.getLogger(__name__)
     allow_channels=False,
 )
 
+    @userge.bot.on_callback_query(filters.regex(pattern=r"^teste_apple$"))
+    async def _teste_apple(_, c_q: CallbackQuery):
+        c_q.from_user.id
+        await c_q.answer(
+            f"𝐀𝐩𝐩𝐥𝐞𝐁𝐨𝐭 𝐓𝐞𝐚𝐦:\n𝚆𝚘𝚛𝚔𝚒𝚗𝚐 𝚘𝚗 𝙱𝚘𝚝\n\n o/",
+            show_alert=True,
+        )
+        return _teste_apple
+
 async def apple(message: Message):
     await message.edit("'**Checando...**\n**Aguarde, Mestre... **`'", log=__name__)
     photo = "https://telegra.ph/file/d50925c35883b16be6cd6.png"
     texto = "**APPLEBOT** is **UP AND RUN**\n\nMain: @applled"
-    await userge.send_photo(message.chat.id, photo=photo, caption=texto)
+    await userge.send_photo(message.chat.id, photo=photo, caption=texto, reply_markup=InlineKeyboardMarkup(buttons))
+    buttons = InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton("TESTE", url=Config.MEUTG_REPO)],
+                [InlineKeyboardButton("TESTE 2", callback_data="teste_apple")],
+            ]
+        )
     del_in=20,
