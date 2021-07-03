f44224d6010fb2bebb88970c7f96ae14b3fce669
Apple <scarotrhy@gmail.com>
2021-07-03 07:16:13 -0300

    Update check.py

--- plugins/check.py
+++ plugins/check.py
@@ -8,7 +8,7 @@ from userge.utils import get_file_id, rand_array
 
 LOGGER = userge.getLogger(__name__)
 # CHECK_P = "https://telegra.ph/file/a9730c950f79c1f06a800.gif"
-CHECK_P = str(CHECK_MEDIA) 
+# CHECK_P = str(CHECK_MEDIA) 
 
 @userge.on_cmd(
     "check",
@@ -21,7 +21,7 @@ CHECK_P = str(CHECK_MEDIA)
 
 async def apple(message: Message):
     await message.edit("**Checando...**\nAguarde, Mestre...", log=__name__)
-#   photo = "https://telegra.ph/file/a47baf0bfed24400c0089.png"
+    photo = "https://telegra.ph/file/a47baf0bfed24400c0089.png"
     texto = "... "
     await userge.send_photo(message.chat.id, CHECK_P, caption=texto)
 
