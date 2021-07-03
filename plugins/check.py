d359a74899bf041ac98086e4b8b4ce620ea89aa9
Apple <scarotrhy@gmail.com>
2021-07-03 07:16:51 -0300

    Update check.py

--- plugins/check.py
+++ plugins/check.py
@@ -23,8 +23,6 @@ async def apple(message: Message):
     await message.edit("**Checando...**\nAguarde, Mestre...", log=__name__)
     photo = "https://telegra.ph/file/a47baf0bfed24400c0089.png"
     texto = "... "
-    await userge.send_photo(message.chat.id, CHECK_P, caption=texto)
+    await userge.send_photo(message.chat.id, photo=photo, caption=texto)
 
     
-    
-    # photo=photo
