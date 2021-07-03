b4182aafafce457cf2c0ec49f542fc6189edecd7
Apple <scarotrhy@gmail.com>
2021-07-03 07:42:09 -0300

    @applled

--- plugins/check.py
+++ plugins/check.py
@@ -7,8 +7,6 @@ from userge import Config, Message, userge
 from userge.utils import get_file_id, rand_array
 
 LOGGER = userge.getLogger(__name__)
-# CHECK_P = "https://telegra.ph/file/a9730c950f79c1f06a800.gif"
-# CHECK_P = str(CHECK_MEDIA) 
 
 @userge.on_cmd(
     "check",
@@ -20,10 +18,9 @@ LOGGER = userge.getLogger(__name__)
 )
 
 async def apple(message: Message):
-    await message.edit("**Checando...**\nAguarde, Mestre...", log=__name__)
+    await message.edit("**Iniciando checagem...**\nAguarde o resultado, Mestre...", log=__name__)
     photo = "https://telegra.ph/file/a9730c950f79c1f06a800.gif"
-    texto = "__I'm Online..."
+    texto = "__I'm Online, @appled!__ Since: {userge.uptime}"
     await message.client.send_animation(message.chat.id, animation=photo, caption=texto)
-#   await userge.send_photo(message.chat.id, photo=photo, caption=texto)
 
-    
+   #Adicionar outras configurações
