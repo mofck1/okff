682e0426b18dba54f39b63c235ef485a59f82f2f
Apple <scarotrhy@gmail.com>
2021-07-03 04:37:13 -0300

    Update online.py

--- plugins/online.py
+++ plugins/online.py
@@ -5,9 +5,6 @@ import asyncio
 from userge import Message, userge
 from userge.utils import get_file_id, rand_array
 
-MEDIA_ON = "https://telegra.ph/file/d50925c35883b16be6cd6.png"
-texto = "**APPLEBOT** is **UP AND RUN**\n\n"
-texto += "Main: @applled"
 
 LOGGER = userge.getLogger(__name__)
 
@@ -21,7 +18,7 @@ LOGGER = userge.getLogger(__name__)
 )
 
 async def apple(message: Message):
-    await message.edit(
-        f'**Checando...**\n**Aguarde, Mestre... **`')
-    try:
-    await userge.send_photo(online.chat_id, MEDIA_ON,caption=texto)
+    await message.edit("'**Checando...**\n**Aguarde, Mestre... **`'", del_in=2)
+    photo = "https://telegra.ph/file/d50925c35883b16be6cd6.png"
+    texto = "**APPLEBOT** is **UP AND RUN**\n\nMain: @applled"
+    await userge.send_photo(message.chat.id, photo=photo, caption=texto)
