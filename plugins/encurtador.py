8b677b8276b25ee8c87696b34f013e0398a9ea99
Apple <scarotrhy@gmail.com>
2021-07-03 23:41:00 -0300

    Update encurtador.py

--- plugins/encurtador.py
+++ plugins/encurtador.py
@@ -17,12 +17,12 @@ from userge.utils import get_file_id, rand_array
 )
  
 async def encurtador(message: Message):
-    if message.fwd_from:
+    if message.forward_message:
         return
     input_str = message.pattern_match.group(1)
     encurta_url = "https://da.gd/s?url={}".format(input_str)
     response_api = requests.get(encurta_url).text
     if response_api:
-        await message.edit("Gerando {} para {}.".format(response_api, input_str))
+        await userge.client.forward_messages("Gerando {} para {}.".format(response_api, input_str))
     else:
         await message.edit("Algo não está certo, tente de novo.")
