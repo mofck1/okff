5c68dc11e9bfd17a50dc1a5ecef8e48cbfb8ef90
Apple <scarotrhy@gmail.com>
2021-07-03 23:28:54 -0300

    Create encurtador.py

---
+++ plugins/encurtador.py
@@ -0,0 +1,28 @@
+""" Módulo de testes para o @applled com fins de aprendizado """
+
+import os
+import asyncio
+import requests
+import json
+from userge import Config, Message, userge
+from userge.utils import get_file_id, rand_array
+
+@userge.on_cmd(
+    "url",
+    about={
+        "header": "Módulo teste para o @applled",
+    },
+    del_pre=True,
+    allow_channels=False,
+)
+ 
+async def encurtador(message: Message):
+    if message.fwd_from:
+        return
+    input_str = message.pattern_match.group(1)
+    encurta_url = "https://da.gd/s?url={}".format(input_str)
+    response_api = requests.get(encurta_url).text
+    if response_api:
+        await message.edit("Gerando {} para {}.".format(response_api, input_str))
+    else:
+        await message.edit("Algo não está certo, tente de novo.")
