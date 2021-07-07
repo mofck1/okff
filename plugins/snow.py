d0faf12d190084aad93c5b25d759f96781d220a5
Apple <scarotrhy@gmail.com>
2021-07-07 07:05:11 -0300

    Update snow.py

--- plugins/snow.py
+++ plugins/snow.py
@@ -8,12 +8,18 @@ from userge.utils.exceptions import StopConversation
 
 @userge.on_cmd("snow", about={
     'header': "Teste do @applled",
-    'como usar': "{tr}snow"}, allow_via_bot=False)
+    'como usar': "{tr}snow"}, allow_via_bot=False, allow_channels=False,)
+
 async def gusta(msg: Message):
     chat = "/now" # msg.input_or_reply_str
     if not chat:
-        await msg.err("@applled")
-        return
+#        await msg.err("@applled")
+#        return
+        await message.err("@applled") # t
+    bot = await userge.bot.get_me()
+    x = await userge.get_inline_bot_results(
+        SpotipieBot, "𝐒𝐏𝐎𝐓𝐈𝐅𝐘 𝐏𝐑𝐄𝐌𝐈𝐔𝐌"
+    ) # t
     try:
         async with userge.conversation("SpotipieBot") as conv:
             await conv.send_message("/start")
@@ -22,8 +28,15 @@ async def gusta(msg: Message):
             FINAL = (
                 await conv.get_response(mark_read=True)
             )
-        await msg.edit(
-            f"𝐒𝐏𝐎𝐓𝐈𝐅𝐘 𝐏𝐑𝐄𝐌𝐈𝐔𝐌\n {FINAL}")                                  
+#        await msg.edit(
+#            f"𝐒𝐏𝐎𝐓𝐈𝐅𝐘 𝐏𝐑𝐄𝐌𝐈𝐔𝐌\n {FINAL}") 
+            await userge.send_inline_bot_result(
+        chat_id=message.chat.id,
+        query_id=x.query_id,
+        result_id=x.results[0].id,
+        reply_to_message_id=replied.message_id,
+    )
+    await message.delete()
     except YouBlockedUser: 
         await msg.edit("Desbloqueie o **@SpotipieBot**")
     except StopConversation:
