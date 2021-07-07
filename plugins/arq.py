30c9fb621ca7b7d7d396d78365a8c390448ab34b
Apple <scarotrhy@gmail.com>
2021-07-07 08:21:08 -0300

    @applled

---
+++ plugins/arq.py
@@ -0,0 +1,32 @@
+""" Módulo de testes para o @applled com fins de aprendizado """
+
+import asyncio
+import os
+from pyrogram.errors import YouBlockedUser
+from userge import Config, Message, userge
+from userge.utils.exceptions import StopConversation
+
+
+@userge.on_cmd("arq", about={
+    'header': "Teste do @applled",
+    'como usar': "{tr}arq"}, allow_via_bot=False, allow_channels=False,)
+
+async def arquivo(msg: Message):
+    chat = msg.input_or_reply_str
+    if not chat:
+        await msg.err("@applled")
+        return
+    try:
+        async with userge.conversation("uploadbot") as conv:
+            await conv.send_message("/start")
+            await conv.get_response(mark_read=True)
+            await conv.send_message(chat)
+            UPLOAD = (
+                await conv.get_response(mark_read=True)
+                try:            
+        await msg.edit(
+            f"Teste\n {UPLOAD}") 
+    except YouBlockedUser: 
+        await msg.edit("Desbloqueie o **@uploadbot**")
+    except StopConversation:
+        await msg.err("O Bot está morto...")
