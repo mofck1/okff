db03413062fd7f5284870a6f1d5195b559b7dbce
Apple <scarotrhy@gmail.com>
2021-07-08 07:13:33 -0300

    Update spot.py

--- plugins/spot.py
+++ plugins/spot.py
@@ -25,7 +25,7 @@ async def spott(msg: Message):
                 msg = await conv.send_message(now)
                 response = await conv.get_response()
                 """ - don't spam notif - """
-                await bot.send_read_acknowledge(conv.chat_id)
+                await self._client.send_read_acknowledge(conv.chat_id)
             except YouBlockedUser:
                 await message.reply("**Por favor desbloqueie** @spotipiebot**.**")
                 return
