7f69ff8277e31309b765904735f28fa2432c8beb
Apple <scarotrhy@gmail.com>
2021-07-12 11:58:05 -0300

    @applled

---
+++ plugins/checking.py
@@ -0,0 +1,35 @@
+""" Módulo de testes para o @applled com fins de aprendizado  """
+
+import os
+import asyncio
+from datetime import datetime
+from PIL import Image
+import asyncio
+import random
+from userge import Config, Message, userge
+from userge.utils import get_file_id, rand_array
+from pyrogram import filters
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+
+LOGGER = userge.getLogger(__name__)
+
+@userge.on_cmd(
+    "checking",
+    about={
+        "header": "Módulo teste para o @applled",
+    },
+    del_pre=True,
+    allow_channels=False,
+    allow_via_bot=True,
+)
+
+async def apple(message: Message):
+    await message.edit("**𝚃𝚎𝚜𝚝𝚎 𝚐𝚎𝚛𝚊𝚍𝚘...**\n𝙰𝚐𝚞𝚊𝚛𝚍𝚎 𝚘 𝚛𝚎𝚜𝚞𝚕𝚝𝚊𝚍𝚘, 𝙼𝚎𝚜𝚝𝚛𝚎...", del_in=1, log=__name__)
+    texto = "@applled"
+    mediag = await userge.bot.get_messages("orugugu", 61)
+    media_id = get_file_id(mediag)  
+    await message.edit_message_media(
+                         media=media_id,
+                         message.chat.id, 
+                         caption=texto,
+    )
