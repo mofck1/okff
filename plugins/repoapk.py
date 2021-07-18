6bbc6daac8895d7a277ac909b3f2fd93be12164c
Apple <scarotrhy@gmail.com>
2021-07-18 09:00:19 -0300

    @applled

---
+++ plugins/repoapk.py
@@ -0,0 +1,38 @@
+""" Buscador de arquivos em APK - Teste do @applled """
+
+from pyrogram.errors import BadRequest
+from userge import Message, userge
+from userge.utils import get_file_id
+
+
+@userge.on_cmd(
+    "apk",
+    about={
+        "titulo": "Pesquisa qualquer APK",
+        "como usar": "apk Spotify",
+    },
+)
+async def apkapplled(message: Message):
+    """ Pesquise qualquer APK """
+    aplicativo = message.input_str
+    if not aplicativo:
+        await message.err("Tente usar o nome de um app.", del_in=10)
+        return
+    search = await message.edit("Pesquisando por: **{}**".format(aplicativo))
+    chat_id = message.chat.id
+    f_id = ""
+    try:
+        async for msg in userge.search_messages(
+            "PrimeApk", query=aplicativo, limit=1, filter="document"
+        ):
+            f_id = get_file_id(msg)
+    except BadRequest:
+        await search.edit(
+            "Obrigatório participar do deste [canal](https://t.me/PrimeApk)."
+        )
+        return
+    if not f_id:
+        await search.edit("Não encontrei foi nada...", del_in=5)
+        return
+    await userge.send_document(chat_id, f_id)
+    await search.delete()
