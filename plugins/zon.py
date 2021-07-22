14900813991b5d245b2de10cae0718b0910fad5a
Apple <scarotrhy@gmail.com>
2021-07-22 04:59:24 -0300

    Create zon.py

---
+++ plugins/zon.py
@@ -0,0 +1,38 @@
+"""Pesquisa simplificada do Google - @applled"""
+
+import requests
+
+from userge import Message, userge
+
+
+@userge.on_cmd(
+    "zon",
+    about={
+        "titulo": "Módulo criado pelo @applled - Amazon",
+        "como usar": "{tr}zon Redmi Note 8",
+    },
+    del_pre=True,
+    allow_channels=False,
+)
+async def pesquisa_amazon(message: Message):
+    """Amazon"""
+    query = message.input_or_reply_str
+    if not query:
+        await message.edit("`Vou pesquisar o vento?!`")
+        return
+    query = query.replace(" ", "+")
+    amazon_url = f"https://amznsearch.vercel.app/api/?query={query}"
+    payload = {"format": "json", "url": amazon_url}
+    r = requests.get("http://is.gd/create.php", params=payload)
+    await message.edit(
+        f"""
+✅ **Este é o resultado da Sua Pesquisa:**
+{query}
+
+"""
+    )
+    
+    
+#  🔗 [{query}]({r.json()['shorturl']})
+#  ➖➖➖➖
+#Dev: @applled
