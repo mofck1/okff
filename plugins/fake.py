98c3ebae111512228a94bfe1760a831ba3b49f80
Apple <scarotrhy@gmail.com>
2021-07-24 08:06:24 -0300

    @applled
    Teste

--- plugins/fake.py
+++ plugins/fake.py
@@ -7,6 +7,11 @@ from userge import Config, Message, userge
 from userge.utils import get_file_id, rand_array
 from pyrogram import filters
 
+RESULTADO = (
+    "https://thispersondoesnotexist.com/image",
+)
+
+
 @userge.on_cmd(
     "fake",
     about={
@@ -32,3 +37,22 @@ async def foto_falsa(message: Message):
             reply_to_message_id=message.reply_to_message.message_id,
         )
   
+
+
+@userge.on_cmd(
+    "fake2",
+    about={
+        "titulo": "Teste",
+        "descrição": "Teste",
+        "como usar": "{tr}fake2",
+    },
+)
+async def falso_teste(message: Message):
+    await message.edit(f"Aguarde...", del_in=5, log=__name__)
+    photo = f"""{random.choice(RESULTADO)}"""
+    texto = f"Foto Fake Gerada\nFonte: ThisPersonDoesntExist.\nDivirta-se ;)"
+    await message.client.send_animation(
+                         message.chat.id, 
+                         animation=photo, 
+                         caption=texto,
+    )
