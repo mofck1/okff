a4cd6fbb8a4e62f7192366e4ade8bcab7c30da2f
Apple <scarotrhy@gmail.com>
2021-07-24 08:07:34 -0300

    Update fake.py

--- plugins/fake.py
+++ plugins/fake.py
@@ -11,34 +11,6 @@ RESULTADO = (
     "https://thispersondoesnotexist.com/image",
 )
 
-
-@userge.on_cmd(
-    "fake",
-    about={
-        "titulo": "Teste",
-        "descrição": "Teste",
-        "como usar": "{tr}fake",
-    },
-)
-
-async def foto_falsa(message: Message):
-    url = "https://thispersondoesnotexist.com/image"
-    response = requests.get(url)
-    if response.status_code == 200:
-        with open("APPLEBOT.jpg", "wb") as f:
-            f.write(response.content)
-    resultado = f"Foto Fake Gerada\nFonte: ThisPersonDoesntExist.\nDivirta-se ;)"
-    fake = "APPLEBOT.jpg"
-    if message.reply_to_message:
-        await client.send_photo(
-            message.chat.id,
-            photo=fake,
-            caption=resultado,
-            reply_to_message_id=message.reply_to_message.message_id,
-        )
-  
-
-
 @userge.on_cmd(
     "fake2",
     about={
