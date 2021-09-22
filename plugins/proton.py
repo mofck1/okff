4aaf598916ff3dd839862bf239a916faa8b5491c
Apple <scarotrhy@gmail.com>
2021-09-22 15:51:50 -0300

    Update proton.py

--- plugins/proton.py
+++ plugins/proton.py
@@ -7,11 +7,6 @@ from userge import Message, userge
 @userge.on_cmd("pro$", about={"título": "Tu sabe o que é :D"}, trigger="", allow_via_bot=False)
 async def proton_msg(message: Message):
     await message.edit(
-        "**-.-. --- -- . -. - .-. .. --- / .--. .-. . -- .. ..- --", del_in=2, log=__name__
-    )
-    texto = f"ⓘ <i>Essa mensagem está disponível apenas para usuários da ProtonAOSP Premium.</i> <u>[Saiba mais](https://bit.ly/protonpremium)</u>"
-    await message.edit(
-        message.chat.id,
-        caption=texto,
-        disable_web_page_preview=True,
-    )
+        "ⓘ <i>Essa mensagem está disponível apenas para usuários da ProtonAOSP Premium.</i> <u>[Saiba mais](https://bit.ly/protonpremium)</u>", disable_web_page_preview=True)
+  
+ 
